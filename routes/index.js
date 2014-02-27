0d7b7ad67bcf0c0647fad1c1cae757020e0d46e9
oolalalaura <lchavess@ucsd.edu>
2014-02-27 10:36:56 -0800

    lab8 fun

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+  	var projects = {'grid': false};
   	res.render('index', projects);
+};
+exports.viewGrid = function(req, res){
+	var projects = {'grid': true};
+	res.render('index', projects);
 };
